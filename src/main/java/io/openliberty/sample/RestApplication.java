7e972cf2ec43f719d1f60b2a9dfc34042a86aae9
thasnim-fluxone <146568184+thasnim-fluxone@users.noreply.github.com>
2025-11-24 20:52:51 +0530

    Delete src/main/java/io/openliberty/sample/RestApplication.java

--- src/main/java/io/openliberty/sample/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package io.openliberty.sample;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")   // <- all REST endpoints start with /api
-public class RestApplication extends Application {
-    // no code needed here
-}
