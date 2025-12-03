492bbbf780362d50bf3e182864df299c40ccd135
thasnim-fluxone <146568184+thasnim-fluxone@users.noreply.github.com>
2025-12-03 12:34:02 +0530

    Update SystemRuntime.java

--- src/main/java/io/openliberty/sample/system/SystemRuntime.java
+++ src/main/java/io/openliberty/sample/system/SystemRuntime.java
@@ -25,7 +25,7 @@ import jakarta.ws.rs.Produces;
 import java.util.Properties;
 
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+// import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
